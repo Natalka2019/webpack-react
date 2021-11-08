9d3b4fe488fbc523c1518556c24c987b97cea48a
Natalka2019 <47332192+Natalka2019@users.noreply.github.com>
2021-11-08 19:24:39 +0200

    uncomment toast on errors (#15)

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -1,10 +1,8 @@
 import axios from "./axiosClient";
 import { IMovie } from "models";
 
-export const getMovies = (requestParams: string) => {
-  console.log(requestParams);
-  return axios.get<IMovie[]>(`movies?${requestParams}`);
-};
+export const getMovies = (requestParams: string) =>
+  axios.get<IMovie[]>(`movies?${requestParams}`);
 
 export const getMovie = (id: number) => axios.get<IMovie[]>(`movies/${id}`);
 
